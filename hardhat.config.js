b98a60c7906c15f0666a135c7fa42c5d84665f30
3illBaby <thrillanon@outlook.com>
2022-11-22 13:47:12 +0100

    3ill Commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+require("@nomicfoundation/hardhat-toolbox");
+const {API_URL, PRIVATE_KEY} = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+};
